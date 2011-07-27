a244d8e328f1dec645d85ffe4c81ab072af31441
Mark Allison <mark@stylingandroid.com>
2011-07-27 14:38:37 +0100

    Initial commit

---
+++ src/com/stylingandroid/dynamiclayouts/DynamicLayoutsActivity.java
@@ -0,0 +1,13 @@
+package com.stylingandroid.dynamiclayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DynamicLayoutsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
